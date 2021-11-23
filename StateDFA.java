ad162701f6de2ef69662efed629a5fca6f0591c4
Devam Patel <Devam29@gmail.com>
2021-11-22 19:15:57 -0800

    comments for sub classes

--- StateDFA.java
+++ StateDFA.java
@@ -1,8 +1,12 @@
+/*
+* StateDFA represents a single state in a DFA
+*/
+
 public class StateDFA {
     
-    public String stateID;
-    public boolean isStart = false;
-    public boolean isAccept = false;
+    public String stateID; // name of the state
+    public boolean isStart = false; // is state initial state?
+    public boolean isAccept = false; // is state accepting state?
 
     public StateDFA(String id, boolean start, boolean accept) { //constructor
         stateID = id;
--- Transition.java
+++ Transition.java
@@ -1,18 +1,21 @@
-
+/*
+* Transition class represents a single edge between two states of a DFA
+*/
 
 public class Transition {
 
-    String source;
-    String alpha;
-    String dest;
+    String source; // source state of the transition
+    String alpha; // transition symbol
+    String dest; // destination state of the transition
 
+    // constructor
     public Transition(String sr, String a, String dest){
         source = sr;
         alpha = a;
         this.dest = dest;
     }
 
-
+    // toString method to format transtions
     public String toString(){
         return source + " " + alpha + " " + dest; 
     }
